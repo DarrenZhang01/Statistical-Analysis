f534fcf4e6dae62535f9a81f5056f1c827ff8ea0
Darren <18633059886@163.com>
2018-07-04 12:54:18 -0400

    integer type in R

--- R/datatype.R
+++ R/datatype.R
@@ -2,6 +2,11 @@
 
 data <- TRUE
 print(class(data))
+
 # 1 is not equivalent to TRUE in R
 data <- 1
+print(class(data))
+
+# Note that integer is different from numeric types in R
+data <- 10L
 print(class(data))
